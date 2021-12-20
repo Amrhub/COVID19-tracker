2e7f37809a185722dcf70bfc0a79385ae58786ae
Amrhub <amr_ahmed65@hotmail.com>
2021-12-20 20:24:15 +0200

    setup: page routes and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+import Home from './pages/Home';
+import Details from './pages/Details';
+
 const App = () => (
-  <div>
-    <h1>Hello World</h1>
-  </div>
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/details" element={<Details />} />
+    </Routes>
+  </>
 );
 
 export default App;
