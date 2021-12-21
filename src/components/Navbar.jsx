a5155bc4029da3f8a1f58f1a5555c81cbdf28bc7
Amrhub <amr_ahmed65@hotmail.com>
2021-12-21 12:21:43 +0200

    feat: style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import Toolbar from '@mui/material/Toolbar';
+import AppBar from '@mui/material/AppBar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+import SearchIcon from '@mui/icons-material/Search';
+import { format } from 'date-fns';
+import { styled } from '@mui/material/styles';
+// import { NavLink } from 'react-router-dom';
+
+const HeaderTypography = styled(Typography)(({ theme }) => ({
+  flexGrow: 1,
+  textAlign: 'center',
+  fontSize: '1rem',
+  [theme.breakpoints.up('sm')]: {
+    fontSize: '1.5rem',
+  },
+}));
+
+const WhiteIconButton = styled(IconButton)(({ theme }) => ({
+  color: theme.palette.common.white,
+}));
 
 const Navbar = () => (
-  <header>
-    <nav>
-      <NavLink to="/">home</NavLink>
-      <NavLink to="/details">details</NavLink>
-    </nav>
-  </header>
+  <AppBar>
+    <Toolbar>
+      <WhiteIconButton>
+        <MenuIcon />
+      </WhiteIconButton>
+      <HeaderTypography variant="h6" color="inherit" noWrap>
+        {format(new Date(), 'do MMMM yyyy')}
+      </HeaderTypography>
+      <WhiteIconButton>
+        <SearchIcon />
+      </WhiteIconButton>
+    </Toolbar>
+  </AppBar>
 );
 
 export default Navbar;
