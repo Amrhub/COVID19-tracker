3021192cd0ec985cea6abf13ca4d4e2f01f9ec86
Amrhub <amr_ahmed65@hotmail.com>
2021-12-21 12:21:37 +0200

    feat: customize theme primary color

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { Route, Routes } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Navbar from './components/Navbar';
 
 import Home from './pages/Home';
 import Details from './pages/Details';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ec4c8a',
+      contrastText: '#fff',
+    },
+  },
+});
+
 const App = () => (
-  <>
+  <ThemeProvider theme={theme}>
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/details" element={<Details />} />
     </Routes>
-  </>
+  </ThemeProvider>
 );
 
 export default App;
