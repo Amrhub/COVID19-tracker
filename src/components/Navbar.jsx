0b13b305c79f8c53606f64f434a2a6d18992b751
Amrhub <amr_ahmed65@hotmail.com>
2021-12-22 17:59:36 +0200

    feat: add date input and functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,39 +2,84 @@ import Toolbar from '@mui/material/Toolbar';
 import AppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import Typography from '@mui/material/Typography';
 import SearchIcon from '@mui/icons-material/Search';
 import { format } from 'date-fns';
 import { styled } from '@mui/material/styles';
-// import { NavLink } from 'react-router-dom';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+import MobileDatePicker from '@mui/lab/MobileDatePicker';
 
-const HeaderTypography = styled(Typography)(({ theme }) => ({
-  flexGrow: 1,
-  textAlign: 'center',
-  fontSize: '1rem',
-  [theme.breakpoints.up('sm')]: {
-    fontSize: '1.5rem',
-  },
-}));
+import { useEffect, useState } from 'react';
+import { Box, TextField } from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { fetchReports } from '../redux/reports/reports';
 
 const WhiteIconButton = styled(IconButton)(({ theme }) => ({
   color: theme.palette.common.white,
 }));
 
-const Navbar = () => (
-  <AppBar>
-    <Toolbar>
-      <WhiteIconButton>
-        <MenuIcon />
-      </WhiteIconButton>
-      <HeaderTypography variant="h6" color="inherit" noWrap>
-        {format(new Date(), 'do MMMM yyyy')}
-      </HeaderTypography>
-      <WhiteIconButton>
-        <SearchIcon />
-      </WhiteIconButton>
-    </Toolbar>
-  </AppBar>
-);
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [searchDate, setSearchDate] = useState(format(new Date(), 'yyyy-MM-dd'));
+
+  useEffect(() => {
+    dispatch(fetchReports(searchDate));
+  }, [searchDate]);
+
+  const updateSearchDate = (date) => {
+    setSearchDate(format(date, 'yyyy-MM-dd'));
+    navigate('/');
+  };
+
+  return (
+    <AppBar color="background">
+      <Toolbar>
+        <WhiteIconButton>
+          <MenuIcon />
+        </WhiteIconButton>
+        <Box
+          sx={{
+            flexGrow: '1',
+            display: 'flex',
+            marginY: 2,
+            justifyContent: 'center',
+          }}
+        >
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <Box sx={{ display: { md: 'none' } }}>
+              <MobileDatePicker
+                inputFormat="yyyy-MM-dd"
+                value={searchDate}
+                onChange={updateSearchDate}
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                renderInput={(params) => <TextField {...params} />}
+                minDate={new Date('2020-01-23')}
+                maxDate={new Date()}
+                disableCloseOnSelect={false}
+              />
+            </Box>
+            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+              <DesktopDatePicker
+                inputFormat="yyyy-MM-dd"
+                value={searchDate}
+                onChange={updateSearchDate}
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                renderInput={(params) => <TextField {...params} />}
+                minDate={new Date('2020-01-23')}
+                maxDate={new Date()}
+              />
+            </Box>
+          </LocalizationProvider>
+        </Box>
+        <WhiteIconButton>
+          <SearchIcon />
+        </WhiteIconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Navbar;
