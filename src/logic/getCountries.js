fa888565088045623cbe805a21b96ebf8f334c57
Amrhub <amr_ahmed65@hotmail.com>
2021-12-23 12:09:17 +0200

    feat: using github repo to get countries image

--- src/logic/getCountries.js
+++ src/logic/getCountries.js
@@ -1,15 +1,6 @@
 import _ from 'lodash';
 
-const desiredCountries = [
-  { name: 'US', path: '/src/assets/us.svg' },
-  { name: 'Spain', path: '/src/assets/spain.svg' },
-  { name: 'United Kingdom', path: '/src/assets/uk.svg' },
-  { name: 'Germany', path: '/src/assets/germany.svg' },
-  { name: 'China', path: '/src/assets/china.svg' },
-  { name: 'France', path: '/src/assets/france.svg' },
-];
-
-const filterCountries = (data) => {
+const formatCountries = (data) => {
   const date = data.dates;
 
   const filteringArr = [];
@@ -17,12 +8,9 @@ const filterCountries = (data) => {
 
   const [{ countries }] = filteringArr;
 
-  _.forEach(desiredCountries, (country) => {
-    countries[country.name].imagePath = country.path;
-    if (country.name === 'US') countries[country.name].name = 'United States';
-  });
-
   return countries;
 };
 
-export default filterCountries;
+export const countriesList = (countries) => _.map(countries, (country) => country.name);
+
+export default formatCountries;
