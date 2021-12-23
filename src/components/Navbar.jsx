2bf164822556d33646b28d706e25a590e8c05ccf
Amrhub <amr_ahmed65@hotmail.com>
2021-12-23 12:12:39 +0200

    feat: add search and date picker functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import Toolbar from '@mui/material/Toolbar';
 import AppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
 import { format } from 'date-fns';
 import { styled } from '@mui/material/styles';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
@@ -10,8 +10,8 @@ import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
 import MobileDatePicker from '@mui/lab/MobileDatePicker';
 
 import { useEffect, useState } from 'react';
-import { Box, TextField } from '@mui/material';
-import { useDispatch } from 'react-redux';
+import { Autocomplete, Box, TextField } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { ArrowBack } from '@mui/icons-material';
 import { fetchReports } from '../redux/reports/reports';
@@ -25,8 +25,10 @@ const Navbar = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const isDetailsPage = /^\/details\/.+/.test(location.pathname);
+  const { countriesList } = useSelector((state) => state.reports);
+  const yesterday = format(new Date(new Date().setDate(new Date().getDate() - 1)), 'yyyy-MM-dd');
 
-  const [searchDate, setSearchDate] = useState(format(new Date(), 'yyyy-MM-dd'));
+  const [searchDate, setSearchDate] = useState(format(new Date(yesterday), 'yyyy-MM-dd'));
 
   useEffect(() => {
     dispatch(fetchReports(searchDate));
@@ -38,52 +40,65 @@ const Navbar = () => {
   };
 
   return (
-    <AppBar color="background">
-      <Toolbar>
-        {isDetailsPage ? (
-          <WhiteIconButton onClick={() => navigate('/')}>
-            <ArrowBack />
-          </WhiteIconButton>
-        ) : null}
-        <Box
-          sx={{
-            flexGrow: '1',
-            display: 'flex',
-            marginY: 2,
-            justifyContent: 'center',
-          }}
-        >
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <Box sx={{ display: { md: 'none' } }}>
-              <MobileDatePicker
-                inputFormat="yyyy-MM-dd"
-                value={searchDate}
-                onChange={updateSearchDate}
-                // eslint-disable-next-line react/jsx-props-no-spreading
-                renderInput={(params) => <TextField {...params} />}
-                minDate={new Date('2020-01-23')}
-                maxDate={new Date()}
-                disableCloseOnSelect={false}
-              />
+    <>
+      {countriesList && (
+        <AppBar color="background">
+          <Toolbar>
+            {isDetailsPage ? (
+              <WhiteIconButton onClick={() => navigate('/')}>
+                <ArrowBack />
+              </WhiteIconButton>
+            ) : null}
+            <Box
+              sx={{
+                flexGrow: '1',
+                display: 'flex',
+                marginY: 2,
+                justifyContent: 'center',
+              }}
+            >
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <Box sx={{ display: { md: 'none' } }}>
+                  <MobileDatePicker
+                    inputFormat="yyyy-MM-dd"
+                    value={searchDate}
+                    onChange={updateSearchDate}
+                    renderInput={(params) => <TextField {...params} />}
+                    minDate={new Date('2020-01-23')}
+                    maxDate={new Date()}
+                    disableCloseOnSelect={false}
+                  />
+                </Box>
+                <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                  <DesktopDatePicker
+                    inputFormat="yyyy-MM-dd"
+                    value={searchDate}
+                    onChange={updateSearchDate}
+                    renderInput={(params) => <TextField {...params} />}
+                    minDate={new Date('2020-01-23')}
+                    maxDate={new Date()}
+                  />
+                </Box>
+              </LocalizationProvider>
             </Box>
-            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-              <DesktopDatePicker
-                inputFormat="yyyy-MM-dd"
-                value={searchDate}
-                onChange={updateSearchDate}
-                // eslint-disable-next-line react/jsx-props-no-spreading
-                renderInput={(params) => <TextField {...params} />}
-                minDate={new Date('2020-01-23')}
-                maxDate={new Date()}
+            {!isDetailsPage && (
+              <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={countriesList}
+                sx={{ width: 250, ml: 2 }}
+                renderInput={(params) => <TextField {...params} label="Country" />}
+                onChange={(e, value) => {
+                  if (value) {
+                    navigate(`/details/${value}`);
+                  }
+                }}
               />
-            </Box>
-          </LocalizationProvider>
-        </Box>
-        <WhiteIconButton>
-          <SearchIcon />
-        </WhiteIconButton>
-      </Toolbar>
-    </AppBar>
+            )}
+          </Toolbar>
+        </AppBar>
+      )}
+    </>
   );
 };
 
