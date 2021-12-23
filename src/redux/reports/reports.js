5d695cee403ca440d246d7f11c5fd84488c509c8
Amrhub <amr_ahmed65@hotmail.com>
2021-12-23 12:11:12 +0200

    feat: add loading screen

--- src/redux/reports/reports.js
+++ src/redux/reports/reports.js
@@ -1,24 +1,32 @@
-import filterCountries from '../../logic/getCountries';
+import _ from 'lodash';
+import formatCountries, { countriesList } from '../../logic/getCountries';
 
 // CONST TYPES
 const FETCH_SUCCEED = 'COVID19TRACKER/REPORTS/FETCH_SUCCEED';
 const FETCH_FAILED = 'COVID19TRACKER/REPORTS/FETCH_FAILED';
+const FETCHING = 'COVID19TRACKER/REPORTS/FETCHING';
 const API_URL = 'https://api.covid19tracking.narrativa.com//api/';
 
 // initial state
-const initialState = [];
+const initialState = { isFetching: true };
 
 // Action Creators
 
 export const fetchReports = (date = '2021-12-21') => async (dispatch) => {
   try {
+    dispatch({ type: FETCHING, payload: true });
     const response = await fetch(API_URL + date);
     const data = await response.json();
 
     data.total.imagePath = '/src/assets/world.svg';
+    const countries = formatCountries(data);
+    const countriesArr = _.map(countries, (country) => country);
     const payload = {
-      countries: filterCountries(data),
+      countries,
+      countriesList: countriesList(countries),
+      countriesArr,
       globalReport: data.total,
+      isFetching: false,
     };
     dispatch({ type: FETCH_SUCCEED, payload });
   } catch {
@@ -29,10 +37,12 @@ export const fetchReports = (date = '2021-12-21') => async (dispatch) => {
 // reducer
 const reducer = (state = initialState, { type, payload }) => {
   switch (type) {
+    case FETCHING:
+      return { ...state, isFetching: payload };
     case FETCH_SUCCEED:
-      return payload;
+      return { ...state, ...payload };
     case FETCH_FAILED:
-      return state;
+      return { state, isFetching: false };
     default:
       return state;
   }
